09cfbfe52abbbbf694f756b90b71595d8cb4342e
Sarah Saballa <127857067+sarahjq-s@users.noreply.github.com>
2023-07-29 18:52:43 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+var countDownDate = new Date("Sept 28, 2023 18:00").getTime();
+
+var x = setInterval(function() {
+
+  var now = new Date().getTime();
+            
+  var distance = countDownDate - now;
+            
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            
+              
+  document.getElementById("countdown").innerHTML = "W e l c o m e  ! " + " " + days  + " days " + " until  Edge of Wander" ;
+  document.getElementById("ticket").innerHTML = "Get your Tickets now!" ;
+ 
+  if (distance < 0) {
+  clearInterval(x);
+  document.getElementById("countdown").innerHTML = "Thank you to everyone who attended this event !";
+  document.getElementById("ticket").innerHTML = "" ;
+
+   }
+ }, 1000); 
+
+
+
 var modal = document.getElementById("video-modal");
 
 var btn = document.getElementById("btn");
